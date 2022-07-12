04602187f777afce19a3029715b3fccd991e8a5d
yeon1615 <aaaask@naver.com>
2022-07-12 16:48:19 +0900

    랜덤숫자

---
+++ 랜덤숫자/app.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form');
+const generatorInput = document.getElementById('generator');
+const guessInput = document.getElementById('guess');
+const choice = document.querySelector('#result p:first-child');
+const result = document.querySelector('#result p:last-child');
+const VALUE_KEY = 'number';
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const generatedValue = parseInt(generatorInput.value);
+  localStorage.setItem(VALUE_KEY, generatedValue);
+  const selectedValue = parseInt(guessInput.value);
+  const savedValue = localStorage.getItem(VALUE_KEY);
+  const randomValue = Math.round(Math.random() * savedValue);
+  choice.innerText = `You chose: ${selectedValue}, the machine chose: ${randomValue}.`;
+  if (selectedValue === randomValue) {
+    result.innerText = 'You won!';
+  } else if (selectedValue !== randomValue) {
+    result.innerText = 'You lost!';
+  }
+}
+
+form.addEventListener('submit', onFormSubmit);
